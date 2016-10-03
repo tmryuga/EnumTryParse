8b8efe1da353a29d5c5b35bb467adee1b117301c
thomasMerant <thomas.merant@alcuin.com>
2016-10-03 12:26:48 +0200

    EnumTryParse

---
+++ EnumTryParse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication27
+{
+    public enum Test
+    {
+        val0 = 0,
+        val1 = 1,
+        val2 = 2,
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result1 = EnumTryParse(null, Test.val0);    // result = val0
+            var result2 = EnumTryParse("val1", Test.val0);  // result = val1
+            var result3 = EnumTryParse(2, Test.val0);       // result = val2
+
+
+            Console.ReadLine();
+        }
+
+        public static TEnum EnumTryParse<TEnum>(object value, TEnum defaultValue)
+        {
+            return value != null && Enum.IsDefined(typeof(TEnum), value) ? (TEnum)Enum.Parse(typeof(TEnum), value.ToString()) : defaultValue;
+        }
+    }
+}
